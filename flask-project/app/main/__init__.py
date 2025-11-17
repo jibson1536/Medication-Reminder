6e2d56aeba95267186514230cf61a8f09723dc29
jibson1536 <153529742+jibson1536@users.noreply.github.com>
2025-11-17 21:46:15 +0100

    Update __init__.py
    init.py

--- flask-project/app/main/__init__.py
+++ flask-project/app/main/__init__.py
@@ -2,4 +2,8 @@ from flask import Blueprint
 
 main = Blueprint('main', __name__)
 
-from . import routes
+# Import route files so Flask loads them
+from app.main import routes_dashboard
+from app.main import routes_medication
+from app.main import routes_profile
+from app.main import routes_settings
