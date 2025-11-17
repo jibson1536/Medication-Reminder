33d3a972acaf3f37b4fbf2c5b30c0678b4b99979
jibson1536 <153529742+jibson1536@users.noreply.github.com>
2025-11-17 21:18:21 +0100

    flask structure
    project flask structure

---
+++ flask-project/app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from app.main import main as main_blueprint
+
+def create_app():
+    app = Flask(__name__)
+    
+    # Register blueprints
+    app.register_blueprint(main_blueprint)
+
+    return app
---
+++ flask-project/config.py
@@ -0,0 +1,6 @@
+class Config:
+    SECRET_KEY = 'your_secret_key_here'
+    DEBUG = True
+    TESTING = False
+    DATABASE_URI = 'sqlite:///site.db'
+    # Add other configuration variables as needed
